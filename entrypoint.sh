56cadbe1aa591cfda860c69ebebcaf18f0011881
mariaurooj <mariaaroj10@gmail.com>
2024-01-01 13:20:32 +0500

    trivy-analysis

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,7 @@ set -e
 apk update
 apk add --no-cache wget gnupg
 
-# Fetch Trivy repository key
-wget -qO /tmp/trivy.gpg https://aquasecurity.github.io/trivy-repo/deb/public.key
-
-# Install Trivy with GPG key verification
-wget -O /etc/apk/keys/trivy.rsa.pub https://aquasecurity.github.io/trivy-repo/deb/trivy.rsa.pub
+# Install Trivy without GPG key verification
 echo "https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 apk add --no-cache trivy
 
