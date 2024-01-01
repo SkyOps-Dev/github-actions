939edfec62a2e748db1be97aec5d59ab36fa6b5d
mariaurooj <mariaaroj10@gmail.com>
2024-01-01 13:14:09 +0500

    trivy-analysis

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,6 @@ set -e
 # Update package lists and install necessary packages using apk
 apk update
 apk add --no-cache wget gnupg
-
-# Create the directory if it doesn't exist
-mkdir -p /usr/share/keyrings/
-
 # Fetch Trivy repository key and add the repository
 wget -qO /usr/share/keyrings/trivy.gpg https://aquasecurity.github.io/trivy-repo/deb/public.key
 
@@ -21,8 +17,4 @@ apk add --no-cache trivy
 # Run Trivy configuration commands if needed
 trivy config "$1"
 
-# Add additional commands or actions you want to perform here
-# ...
 
-# Your entrypoint logic ends here. You might want to add the command that starts your application.
-exec "$@"
