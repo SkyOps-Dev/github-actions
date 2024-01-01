82242d6f3128c354f0d43848a0ccd3e0123175ec
mariaurooj <mariaaroj10@gmail.com>
2024-01-01 12:56:28 +0500

    trivy-analysis

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,10 @@ apk update
 apk add --no-cache wget gnupg
 
 # Fetch Trivy repository key and add the repository
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
-            
+wget -qO /usr/share/keyrings/trivy.gpg https://aquasecurity.github.io/trivy-repo/deb/public.key
+
+# Add Trivy repository to sources list
+echo "https://aquasecurity.github.io/trivy-repo/deb" >> /etc/apk/repositories
 
 # Install Trivy
 apk add --no-cache trivy
