4d0231544f3a0607ee45d1f6b39a8296c0ddfacf
mariaurooj <mariaaroj10@gmail.com>
2024-01-05 10:53:00 +0500

    ecr

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,13 @@
-FROM amazon/aws-cli:latest
+FROM docker:latest
+
+# Install AWS CLI
+RUN apk --no-cache add \
+    python3 \
+    py3-pip \
+    && pip3 install --upgrade pip \
+    && pip3 install awscli
+
 COPY entrypoint.sh /entrypoint.sh
+
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/sh -l
 
-# Set AWS credentials
-aws configure set aws_access_key_id "$1"
-aws configure set aws_secret_access_key "$2"
-aws configure set default.region "$3"
+AWS_ACCESS_KEY_ID=$1
+AWS_SECRET_ACCESS_KEY=$2
+AWS_REGION=$3
+ECR_REPOSITORY=$4
+REGISTRY_TYPE=$5
+IMAGE_NAME=$6
+IMAGE_TAG=$7
 
-# Login to ECR
-aws ecr get-login-password --region "$3" | docker login --username AWS --password-stdin "$4"
-sudo apt-get update -y
-sudo apt install -y docker 
-sudo service docker start 
-docker -v
-# Build and push the Docker image
-docker build -t "$4" .
-docker tag "$4":latest "$4":latest
-docker push "$4":latest
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-echo "ECR login and push complete"
+docker build -t $IMAGE_NAME:$IMAGE_TAG .
+
+docker push $IMAGE_NAME:$IMAGE_TAG
+
+#rep name(public/private)
+#rep type
+#region
+#image tag
