d15938dc0e06b70090d02dc84f6dca7a59ba81c2
mariaurooj <mariaaroj10@gmail.com>
2024-01-05 15:45:07 +0500

    ecr

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -l
 
-AWS_ACCOUNT_ID=$1
-AWS_REGION=$2
-ECR_REPOSITORY=$3
-REGISTRY_TYPE=$4
-IMAGE_NAME=$5
-IMAGE_TAG=$6
+AWS_ACCESS_KEY_ID=$1
+AWS_SECRET_ACCESS_KEY=$2
+AWS_REGION=$3
+ECR_REPOSITORY=$4
+REGISTRY_TYPE=$5
+IMAGE_NAME=$6
+IMAGE_TAG=$7
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
-
+aws configure
 docker build -t $IMAGE_NAME:$IMAGE_TAG .
-
 docker push $IMAGE_NAME:$IMAGE_TAG
