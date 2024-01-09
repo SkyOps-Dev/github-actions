613f036685d672dc1db5182151fcc4c025830141
mariaurooj <mariaaroj10@gmail.com>
2024-01-09 13:51:44 +0500

    ecr

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ REGISTRY_TYPE=$5
 IMAGE_NAME=$6
 IMAGE_TAG=$7
 AWS_ACCOUNT_ID=$8
-
-aws ecr get-login-password --region $AWS_REGION | docker login -u AWS --password-stdin 991108593442.dkr.ecr.us-east-1.amazonaws.com/test
+docker login -u mariaurooj -p mariaarooj10@gmail.com
+aws ecr get-login-password --region $AWS_REGION | docker login -u mariaurooj --password-stdin 991108593442.dkr.ecr.$AWS_REGION.amazonaws.com/test
 docker run -d -it --privileged $IMAGE_NAME:$IMAGE_TAG /usr/sbin/init
 systemctl start httpd.service
 docker build -t $IMAGE_NAME:$IMAGE_TAG .
